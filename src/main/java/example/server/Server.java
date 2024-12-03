0ac347e13b7acbd1c5540fd7a772a4d417bcebd7
Daniel Lewandowski <daniel.lewandowski+agh@outlook.com>
2024-12-03 15:14:47 +0100

    fixed dummy key

--- src/main/java/example/server/Server.java
+++ src/main/java/example/server/Server.java
@@ -31,7 +31,7 @@ public class Server {
 
     private final Map<Request.Authorize, Player.HumanPlayer> known = Map.of(
             new Request.Authorize("1234"), new Player.HumanPlayer("Player 0"),
-            new Request.Authorize("1234"), new Player.HumanPlayer("Player 1"),
+            new Request.Authorize("4231"), new Player.HumanPlayer("Player 1"),
             new Request.Authorize("1345"), new Player.HumanPlayer("Player 2"),
             new Request.Authorize("1678"), new Player.HumanPlayer("Player 3"),
             new Request.Authorize("1679"), new Player.HumanPlayer("Player 4")
