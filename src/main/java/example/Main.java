75ae7da871952ba9c4b801aa3ef94c16474f5a3c
Daniel Lewandowski <daniel.lewandowski@outlook.com>
2024-12-26 20:36:22 +0100

    added better maze generator

--- src/main/java/example/Main.java
+++ src/main/java/example/Main.java
@@ -1,6 +1,7 @@
 package example;
 
 import example.domain.game.Cave;
+import example.domain.game.DrunkenCave;
 import example.domain.game.SimpleCave;
 import example.game.Game;
 import example.server.Server;
@@ -9,7 +10,8 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        final var cave = new SimpleCave(60, 160);
+//        final var cave = new SimpleCave(60, 160);
+        final var cave = new DrunkenCave(60, 160);
         final var game = new Game(cave);
         final var server = new Server(game);
         server.start(8080);
